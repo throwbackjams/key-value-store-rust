0c15adcc64fa0b0b406c4fcd3a0588c3d33e934c
throwbackjams <yli17@alumni.nd.edu>
2022-04-22 12:58:10 -0500

    setup cli commands get set and rm

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,18 +1,45 @@
 #[macro_use]
 extern crate clap;
 use clap::{App, Arg};
+use std::process;
 
 fn main() {
-    // let cli = App:new("kvs")
-    //     .about("CLI for a key value store in rust")
-    //     .version("v0.0.1")
-    //     .author("James Li")
-    //     .arg(Arg::with_name("get"))
-    //     .arg(Arg::with_name("set"))
-    //     .arg(Arg::with_name("rm"))
 
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
+
+    match matches.subcommand() {
+        None => {
+            eprintln!("Must contain valid subcommand or arguments");
+            process::exit(1);
+        },
+        Some(("set", argmatches)) => {
+            eprintln!("unimplemented");
+
+            let key = argmatches.value_of("key").unwrap();
+            let value = argmatches.value_of("value").unwrap();
+
+            println!("Key value pair to be set {:?} : {:?}", key, value);
+            process::exit(1);
+        },
+        Some(("get", argmatches)) => {
+            eprintln!("unimplemented");
+
+            let key = argmatches.value_of("key").unwrap();
+
+            println!("Key to be searched {:?}", key);
+            process::exit(1);
+        },
+        Some(("rm", argmatches)) => {
+            eprintln!("unimplemented");
+
+            let key = argmatches.value_of("key").unwrap();
+
+            println!("Key to be removed {:?}", key);
+            process::exit(1);
+        },
+        _ => unreachable!(),
+    }
 
 
 }
