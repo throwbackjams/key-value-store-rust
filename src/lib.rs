3ce9a6eb557a1266552992ccf418b3e6207409ab
throwbackjams <yli17@alumni.nd.edu>
2022-04-22 11:13:19 -0500

    complete project 1 part 1 compile tests

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+pub struct KvStore{
+}
+
+impl KvStore{
+
+    pub fn new() -> KvStore {
+        KvStore{}
+    }
+
+    pub fn set(&mut self, key: String, value: String) {
+        unimplemented!()
+    }
+
+    pub fn get(&self, key: String) -> Option<String> {
+        unimplemented!()
+    }
+
+    pub fn remove(&mut self, key: String) {
+        unimplemented!()
+    }
+
+}
