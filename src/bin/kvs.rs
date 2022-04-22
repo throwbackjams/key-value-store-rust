04358481e62874c3508b4967bcdee6891df8680d
throwbackjams <yli17@alumni.nd.edu>
2022-04-22 12:13:20 -0500

    init cli via clap and add subcommand structure via yaml

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,18 @@
+#[macro_use]
+extern crate clap;
+use clap::{App, Arg};
+
 fn main() {
-    println!("GM");
+    // let cli = App:new("kvs")
+    //     .about("CLI for a key value store in rust")
+    //     .version("v0.0.1")
+    //     .author("James Li")
+    //     .arg(Arg::with_name("get"))
+    //     .arg(Arg::with_name("set"))
+    //     .arg(Arg::with_name("rm"))
+
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+
 }
