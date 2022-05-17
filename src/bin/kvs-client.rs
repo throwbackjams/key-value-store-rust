75f2decba8dd148ff8807abdce75d217c0358b13
throwbackjams <yli17@alumni.nd.edu>
2022-05-17 13:20:11 -0500

    add address option on client cli; minor edits in server cli

--- src/bin/kvs-client.rs
+++ src/bin/kvs-client.rs
@@ -2,28 +2,52 @@ use clap::Parser;
 use kvs::{KvStore, KvsError, Result};
 use std::path::PathBuf;
 use std::process;
+use tracing::{info, trace};
+use tracing_subscriber;
+use std::net::{IpAddr, TcpListener, TcpStream, Ipv4Addr, Ipv6Addr};
+
+const DEFAULT_IP_ADDRESS: &str = "127.0.0.1:4000";
+
+#[derive(Debug, Parser)]
+#[clap(author, version, about)]
+struct Cli{
+    #[clap(subcommand)]
+    command: Option<Command>,
+}
 
 #[derive(Debug, Parser)]
 #[clap(author, version, about)]
 enum Command {
     ///Set the value of a string key to a string
     Set {
+        ///The key to be set
         #[clap(required = true)]
         key: String,
+        ///The value to be set
         #[clap(required = true)]
         value: String,
+        ///Optional IP:PORT target
+        #[clap(short, long, default_value_t = String::from(DEFAULT_IP_ADDRESS))]
+        addr: String,
     },
     ///Get the string value of a given string key
     Get {
         #[clap(required = true)]
         key: String,
+        ///Optional IP:PORT target
+        #[clap(short, long, default_value_t = String::from(DEFAULT_IP_ADDRESS))]
+        addr: String,
     },
     ///Removes a given key
     Rm {
         #[clap(required = true)]
         key: String,
+        ///Optional IP:PORT target
+        #[clap(short, long, default_value_t = String::from(DEFAULT_IP_ADDRESS))]
+        addr: String,
     },
 }
+
 
 fn main() -> Result<()> {
     let path = PathBuf::from("");
@@ -33,13 +57,33 @@ fn main() -> Result<()> {
 
     // println!("In memory KV: {:?}", in_mem_kv.kv);
 
-    let command = Command::parse();
+    let subscriber = tracing_subscriber::FmtSubscriber::new();
 
-    match command {
-        Command::Set { key, value } => {
+    let _result = tracing::subscriber::set_global_default(subscriber)
+    .map_err(|_err| eprintln!("Unable to set global default subscriber"));
+
+    let cli = Cli::parse();
+
+    match cli.command {
+        Some(Command::Set { key, value, addr }) => {
             // println!("Key value pair to be set {:?} : {:?}", key, value);
 
+            //TODO! Insert function to handle IP parsing
+
+            // let ip2 = IpAddr::from_str(addr)?;
+            // println!("ip2: {:?}", ip2);
+            
+            // let ip: IpAddr;
+
+            // match addr {
+            //     Some(addr) => ip = IpAddr::from(addr),
+            //     None => ip = IpAddr::from("127.0.0.1:4000")?,
+            // };
+            
+            info!("IP Address target: {:?}", addr);
+
             let result = in_mem_kv.set(key, value);
+
 
             match result {
                 Ok(()) => process::exit(0),
@@ -49,7 +93,7 @@ fn main() -> Result<()> {
                 }
             }
         }
-        Command::Get { key } => {
+        Some(Command::Get { key, addr }) => {
             let result = in_mem_kv.get(key);
             // .map_err(|error| {
             //     if let KvsError::Store(err) = error {
@@ -61,6 +105,8 @@ fn main() -> Result<()> {
             //      println!("{}", result.unwrap());
             //  });
 
+            info!("IP Address target: {:?}", addr);
+
             match result {
                 Ok(Some(value)) => println!("{}", value),
                 Ok(None) => println!("Key not found"),
@@ -69,7 +115,10 @@ fn main() -> Result<()> {
 
             process::exit(0);
         }
-        Command::Rm { key } => {
+        Some(Command::Rm { key, addr }) => {
+            
+            info!("IP Address target: {:?}", addr);
+
             let _result = in_mem_kv.remove(key).map_err(|error| {
                 if let KvsError::Store(err) = error {
                     println!("{}", err);
@@ -80,6 +129,7 @@ fn main() -> Result<()> {
 
             process::exit(0);
         }
+        None => {Ok(())}
     }
 
     // todo!("implement result type")
--- src/bin/kvs-server.rs
+++ src/bin/kvs-server.rs
@@ -2,6 +2,7 @@ use clap::Parser;
 // use kvs::{KvStore, KvsError, Result};
 use tracing::{info, trace};
 use tracing_subscriber;
+use std::net::{TcpListener, TcpStream};
 
 
 #[derive(Debug, Parser)]
@@ -33,11 +34,26 @@ fn main() {
         None => ip = String::from("127.0.0.1:4000"),
     };
 
-    info!("Running kvs-server version: {}", env!("CARGO_PKG_VERSION"));
+    //TODO! Handle IP Address / Port Parsing & Error
+
     info!("Listening on IP Address:Port: {}", ip);
+    let listener = TcpListener::bind(ip).unwrap(); //TODO! Better error handling
+    
+    info!("Running kvs-server version: {}", env!("CARGO_PKG_VERSION"));
     info!("Engine used: {:?}", cli.engine.as_deref());
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        info!("Connection established with stream: {:?}", stream);
+
+        handle_connection(stream);
+    }
 
     // println!("addr: {:?}", ip);
     // println!("engine: {:?}", cli.engine.as_deref());
 
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    info!("Inside handle_connection");
 }
